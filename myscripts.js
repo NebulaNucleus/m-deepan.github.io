8db1a6af61782ac8f3a0985d58a1b8159e80a990
Deepan <deepan9942@gmail.com>
2024-01-13 11:58:22 +0530

    added template of login and register page

---
+++ myscripts.js
@@ -0,0 +1,6 @@
+function redirectToLogin() {
+   window.location.href = "/login-page/login.html";
+ }
+ function redirectToRegister() {
+    window.location.href = "/register-page/register.html";
+  }
