e1f39f49c3f47dbd84e407b7b4c8fbe3582a6f3e
Deepan <deepan9942@gmail.com>
2024-01-13 19:29:27 +0530

    redirect page

--- register-page/register.js
+++ register-page/register.js
@@ -1,5 +1,6 @@
 function registerUser() {
 
-    alert("This is sample page !!! not fully completed");
+    alert("This is sample page !!! Not fully completed. You will redirected to main page");
+    window.location.href = "/m-deepan.github.io/";
 
 }
